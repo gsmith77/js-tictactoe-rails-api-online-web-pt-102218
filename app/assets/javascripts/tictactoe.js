108a887a3397d6ddc7553706be48809ae5e9c502
github username <github email address>
2019-06-18 15:47:45 -0500

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,1 +1,25 @@
 // Code your JavaScript / jQuery solution here
+//global variables
+
+let turn = 0
+let stocks = []
+//functions
+
+function player() {
+    if(turn % 2 === 0) {
+        turn += 1
+        return "X";
+    }
+    else {
+        turn += 1
+        return "O";
+    };
+};
+
+function updateState(square) {
+   square.innerHTML = player()
+};
+
+function setMessage() {
+
+};
